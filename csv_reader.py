d09a25b2bc9574e0ff2ef3ae6c30b35c278ff945
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2020-10-16 17:43:37 +0300

    Delete csv_reader.py

--- csv_reader.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def get_data(file_path):
-    data = [row for row in csv.reader(
-        open(file_path, newline='', encoding="utf8"), delimiter=";")]
-    del data[0]
-    return data
